c866c1044c32a024a88bec06b090efbdc0ecbaa4
raylexlee <raylexym57lee@gmail.com>
2020-10-18 14:11:01 +0800

    Add const bashEscapePattern

--- getShScript.js
+++ getShScript.js
@@ -5,6 +5,8 @@ module.exports = arg => {
     const album = arg.albumObj;
     const filename = album.linktitle
     const collectionPath = '/media/raylex/data/DownloadFromYoutube/collection';
+    const bashEscapePattern = /[\]\['&!?,\s()]/g;
+    const escapePattern = bashEscapePattern;
     const pattern = new RegExp(album.regex);
     const hasTime = /\\d/.test(album.regex);
     const TimeTitles = album.timetitles.map(timetitle => {
@@ -14,7 +16,7 @@ module.exports = arg => {
         r[1] = r[1] ? r[1] : '0';
         if (!hasTime) {
             // No timestamp of the songs : This means mp3splt -s works perfect for this album:)
-            return {Time: '', Title: r[1].replace(/[\]\['&\s()]/g, '\\$&')};
+            return {Time: '', Title: r[1].replace(escapePattern, '\\$&')};
         }
         r[2] = r[2] ? r[2] : '0';
         if (/^\d/.test(r[1])) {
@@ -29,11 +31,11 @@ module.exports = arg => {
                 : `${r[2]}.${r[3]}`;
         }
         TitleStr = Title ? Title : '';
-        return {Time: Time, Title: TitleStr.replace(/[\]\['&\s()]/g, '\\$&')};
+        return {Time: Time, Title: TitleStr.replace(escapePattern, '\\$&')};
     });
     const Times = TimeTitles.map(e => e.Time);
-    const artist = album.singer.replace(/[\]\['&\s()]/g, '\\$&');
-    const alBum = album.album.replace(/[\]\['&\s()]/g, '\\$&');
+    const artist = album.singer.replace(escapePattern, '\\$&');
+    const alBum = album.album.replace(escapePattern, '\\$&');
     const Titles = TimeTitles.map( (e, idx) => 
         (idx === 0 )  ? `%[@a=${artist},@b=${alBum},@t=${e.Title},@g=13]` : `[@t=${e.Title}]`);
     const TimeArg = `${Times.join(" ")} EOF`;
@@ -42,7 +44,7 @@ module.exports = arg => {
     const mkdirLine = `mkdir ~/NewMusic/${artist}`;
     const dlmp3Line = `youtube-dl --extract-audio --audio-format mp3 --audio-quality 5 -o 'album.%(ext)s' ${link}`;
     const hasMp3 = fs.existsSync(AlbumCollectionPath);
-    const albumFile = hasMp3 ? `${AlbumCollectionPath.replace(/[\]\['&\s()]/g, '\\$&')}`: 'album.mp3';
+    const albumFile = hasMp3 ? `${AlbumCollectionPath.replace(escapePattern, '\\$&')}`: 'album.mp3';
     const spltLine = hasTime 
       ? `mp3splt -o @a-@t -d ~/NewMusic/${artist} -g ${TitleArg} ${albumFile} ${TimeArg}`
       : `mp3splt -s -o @a-@t -d ~/NewMusic/${artist} -g ${TitleArg} ${albumFile}`;
